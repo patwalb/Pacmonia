d4e1a7ead43c491d1ac9a5eaf68da18f94bb7374
patwalb <31938703+patwalb@users.noreply.github.com>
2017-09-30 23:57:53 +0200

    Formatting and adding comments in pacmonia.py
    Better structure, more overview

--- pacmonia.py
+++ pacmonia.py
@@ -1,33 +1,33 @@
-"""
-Created on Sat Sep 30 00:10:29 2017
-
-@author: Rafael
-"""
-
 import pygame
 import time
 import numpy as np
 import random as rd
 
-########################## Variablen
+########################## Variables
+
+
+# b is a list storing the bacteria
+# m is a list storing the phages
+# p is the player
+
 
 pygame.init()
 
 display_size = 1300
-gameover=False
+gameover     = False
 
-black = (0,0,0)
+black = (  0,  0,  0)
 white = (255,255,255)
-red = (255,0,0)
-blue = (0,0,255)
-green = (0,255,0)
+red   = (255,  0,  0)
+blue  = (  0,  0,255)
+green = (  0,255,  0)
 
 #ckfield = np.zeros((display_size/10,display_size/10))
 
 gameDisplay = pygame.display.set_mode((display_size,display_size))
 pygame.display.set_caption('pacmonia')
 
-########################## Funktionen
+########################## Functions
 
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
@@ -48,26 +48,30 @@ def crash(string):
 def collision(p,b,m):
     global gameover
     global totalcounter
-    tobremove=[]
-    
+    tobremove=[] # list of bacteria to remove (eaten by phages)
+
+    # for all bacteria
     for i in range(len(b)):
-        if p.rect.colliderect(b[i].rect):
+        if p.rect.colliderect(b[i].rect): # bacteria overlay with player
             #tobremove.append(i)
-            totalcounter+=1
+            totalcounter += 1
         for j in range(len(m)):    
-            if m[j].rect.colliderect(b[i].rect):
+            if m[j].rect.colliderect(b[i].rect): # bacteria overlay with phages
                 tobremove.append(i)
-                totalcounter+=1
-                m[j].counter +=1
-                m[j].timer=time.time()
-    
-    for i in range(len(m)):
-        if m[i].rect.colliderect(p.rect):
-            gameover=True
+                totalcounter += 1
+                m[j].counter += 1
+                m[j].timer = time.time()
+
+    # for all phages
+    for i in range(len(m)): 
+        if m[i].rect.colliderect(p.rect): # phages collide with player
+            gameover = True
             crash('You lose')
     
-    tomremove=[]
+    tomremove=[] # list of phages to remove (timeout)
+    # for every phage
     for i in range(len(m)):
+        # if maxtime has been exceeded
         if time.time() - m[i].timer > phage.maxtime:
             tomremove.append(i)
     tomremove.sort(reverse=True)
@@ -76,27 +80,27 @@ def collision(p,b,m):
             
     
     length = len(m)
-    for i in range(length):
-        if m[i].counter > phage.maxeat:
-            m.append(phage(int(rd.randrange(0,display_size-phage.size)),int(rd.randrange(0,display_size-phage.size))))
-            m[i].counter=0
+    for i in range(length): # for all phages
+        if m[i].counter > phage.maxeat: # phage is overeaten
+            m.append(phage(int(rd.randrange(0,display_size-phage.size)),int(rd.randrange(0,display_size-phage.size)))) # create a new phage
+            m[i].counter=0 # set counter of overeaten phage to zero
     
     
     tobremove.sort(reverse=True)
     for i in range(len(tobremove)):
-        b.remove(b[tobremove[i]])
+        b.remove(b[tobremove[i]]) # kill all bacterias that have been eaten
         
-    if len(b)==0:
+    if len(b)==0: # no bacterium existing anymore ... LOST
         gameover=True
         crash('You lose')
-    elif len(b) > 2e5:
+    elif len(b) > 2e5: # too much bacteria existing ... LOST
         gameover=True
         crash('You lose')
 
 def display_highscore(_time):
     global totalcounter
     text = 'Highscore: ' + str(round(_time,2))
-    largeText = pygame.font.Font('freesansbold.ttf',20)
+    largeText = pygame.font.Font('freesansbold.ttf',20) # TODO monospace
     TextSurf, TextRect = text_objects(text, largeText)
     TextRect.center = ((display_size/2),(display_size/20))
     gameDisplay.blit(TextSurf, TextRect)
@@ -107,24 +111,24 @@ def multiply(b):
         b.append(bacteria(b[i].x+bacteria.size, b[i].y))
 
 
-A = 100
+A = 100 
 B = 1
 
-def velocity(dist):
-    return A*dist/(B+dist)
+def velocity(dist): 
+    return A*dist/(B+dist) # given algorithm
 
-########################## Klassen
+########################## Classes
 
 class phage(pygame.sprite.Sprite):
-    size = 20
-    speed = 100
-    radius = 200
-    maxeat = 50
+    size    = 20
+    speed   = 100
+    radius  = 200
+    maxeat  = 50
     maxtime = 20
     
     def __init__(self,x,y):
-        self.x = x
-        self.y = y #int(rd.randrange(0,display_size-self.size))
+        self.x  = x
+        self.y  = y #int(rd.randrange(0,display_size-self.size))
         self.vx = 0
         self.vy = 0
         
@@ -132,12 +136,12 @@ class phage(pygame.sprite.Sprite):
         self.im = pygame.Surface([self.size,self.size])
         self.im.fill(blue)
         
-        self.rect = self.im.get_rect()
+        self.rect   = self.im.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
         
-        self.counter=0
-        self.timer=time.time()
+        self.counter = 0
+        self.timer = time.time()
         
     
     def draw(self):
@@ -147,23 +151,24 @@ class phage(pygame.sprite.Sprite):
             
     def update(self,dt,b):
         
-        #sucht nach bakterien im KÜRZESTEN Abstand
-        distances=[]
+        #searching for bacteria in shortest distance
+        distances = []
         for i in range(len(b)):
             distance = np.sqrt((self.x-b[i].x)**2 + (self.y-b[i].y)**2)
             distances.append(distance)
         
-        c=min(distances)
+        c = min(distances)
         ind = distances.index(c)
         
         if distances[ind] < phage.radius and rd.random()<0.85:
+            #move towards bacterium
             self.vx = (velocity(distances[ind])/distances[ind])* (b[ind].x - self.x)
             self.vy = (velocity(distances[ind])/distances[ind])* (b[ind].y - self.y)
         else:
+            #move to random direction
             self.vx = int(rd.randrange(-self.speed,self.speed))
             self.vy = int(rd.randrange(-self.speed,self.speed))
                 
-        
         self.x += self.vx*dt
         self.y += self.vy*dt
         
@@ -173,15 +178,16 @@ class phage(pygame.sprite.Sprite):
 class bacteria(pygame.sprite.Sprite):
     
     speed = 30
-    size = 10
+    size  = 10
     
     def __init__(self,x,y):
-        self.x = x
-        self.y = y #int(rd.randrange(0,display_size-self.size))
+        self.x  = x
+        self.y  = y #int(rd.randrange(0,display_size-self.size))
         self.vx = 0
         self.vy = 0
         
         if rd.random() < 0.33:
+            # randomn spawn
             self.x = int(rd.randrange(0,display_size-bacteria.size))
             self.y = int(rd.randrange(0,display_size-bacteria.size))
         
@@ -212,7 +218,7 @@ class bacteria(pygame.sprite.Sprite):
 class player(pygame.sprite.Sprite):
     
     speed = 120
-    size = 40
+    size  = 40
     
     def __init__(self):
         self.x = display_size/2
@@ -233,7 +239,7 @@ class player(pygame.sprite.Sprite):
         global gameDisplay
         gameDisplay.blit(self.im,(self.x,self.y))
         
-    
+    # arrow inputs
     def inputs(self, keys):
         if keys[pygame.K_LEFT]:
             self.vx = -self.speed
@@ -261,36 +267,38 @@ class player(pygame.sprite.Sprite):
         #if (self.x > display_size - self.width or self.x < 0) or (self.y < 0 or display_size - self.height < self.y):
         #    self.crash()
 
-N = 10
+N = 10 # counter of bacteria at start time
 totalcounter=0
 
 def gameloop():
     #global ckfield
     global totalcounter
     
-    oldTime=0
-    newTime=0
-    newTime=time.time()
-    gameover=False
-    totalcounter=0
+    oldTime      = 0
+    newTime      = 0
+    newTime      = time.time()
+    gameover     = False
+    totalcounter = 0
     
-    t1 = time.time()
-    t2 = t1
-    _time=0
-    multiplytime=0
+    t1           = time.time()
+    t2           = t1
+    _time        = 0
+    multiplytime = 0
     
     gameExit = False
     pygame.key.set_repeat(50,50)
     
     p = player()
-    m = []
-    m.append(phage(int(rd.randrange(0,display_size-phage.size)),int(rd.randrange(0,display_size-phage.size))))
+    m = [] # list storing all phages
+    # add three pages with random position to start screen/list m
+    m.append(phage(int(rd.randrange(0,display_size-phage.size)),int(rd.randrange(0,display_size-phage.size)))) 
     m.append(phage(int(rd.randrange(0,display_size-phage.size)),int(rd.randrange(0,display_size-phage.size))))
     m.append(phage(int(rd.randrange(0,display_size-phage.size)),int(rd.randrange(0,display_size-phage.size))))
     
-    b=[]
+    b=[] # list storing all bacteria
     for _ in range(N):
-        b.append(bacteria(int(rd.randrange(0,display_size-bacteria.size)),int(rd.randrange(0,display_size-bacteria.size))))
+        # add N bacteria randomly to start screen/list b
+        b.append(bacteria(int(rd.randrange(0,display_size-bacteria.size)),int(rd.randrange(0,display_size-bacteria.size)))) 
     
     
     while not gameExit:
@@ -347,8 +355,7 @@ def gameloop():
         time.sleep(1/80)
 
 
-########################## Spiel
-# Hier gehts los
+########################## Game
 gameloop()
 
 
